dce0d149a87cc77c37005333d28d5012e549f592
Josh Bender <jbendercode@gmail.com>
2016-09-13 13:49:30 +0000

    Initial commit

---
+++ Scripts/core/game.js
@@ -0,0 +1,31 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var Person = (function () {
+    function Person(name) {
+        this._name = name;
+    }
+    Person.prototype.speak = function () {
+        console.log(this._name + " says hello");
+    };
+    return Person;
+}());
+var Student = (function (_super) {
+    __extends(Student, _super);
+    function Student(name, studentNum) {
+        _super.call(this, name);
+        this._studentNum = studentNum;
+    }
+    Student.prototype.studies = function () {
+        console.log(this._name + " is studying");
+    };
+    return Student;
+}(Person));
+var person = new Person("Josh");
+person.speak();
+var student = new Student("Jacky", 300123456);
+student.speak();
+student.studies();
+//# sourceMappingURL=game.js.map
